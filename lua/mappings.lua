c1ab42b129fc7b40ea00dc7636ff4ff7d69befa4
Sibindra <timalsinasibindra@gmail.com>
2025-07-30 20:26:22 +0545

    Git utils

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,20 +1,39 @@
 require "nvchad.mappings"
-
 local map = vim.keymap.set
 
+-- Copilot
 map("i", "<C-l>", function()
     vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
 end, { desc = "Copilot Accept", noremap = true, silent = true })
 
+-- General
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
--- Code action
+-- LSP Actions
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-
--- Find refrences
-map("n", "<leader>fr", ":lua vim.lsp.buf.references()<CR>", { desc = "Find References" })
+map("n", "<leader>fr", ":Telescope lsp_references<CR>", { desc = "Find References" })
+map("n", "gd", ":lua vim.lsp.buf.definition()<CR>", { desc = "Go to Definition" })
 map("n", "<C-LeftMouse>", ":lua vim.lsp.buf.definition()<CR>", { desc = "Cmd+Click Definition" })
 
--- Search and replace
+-- Search and Replace (Spectre)
 map("n", "<leader>sr", ":Spectre<CR>", { desc = "Search and Replace (Project-Wide)" })
+
+-- Git (Gitsigns)
+map("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk" })
+map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+map("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "Blame Line" })
+map("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Inline Blame" })
+map("n", "<leader>gn", ":Gitsigns next_hunk<CR>", { desc = "Next Hunk" })
+map("n", "<leader>gp", ":Gitsigns prev_hunk<CR>", { desc = "Previous Hunk" })
+map("n", "<leader>gd", ":Gitsigns diffthis<CR>", { desc = "Diff This File (Index)" })
+map("n", "<leader>gm", ":Gitsigns diffthis origin/main<CR>", { desc = "Diff This File (origin/main)" })
+map("n", "<leader>gh", ":Gitsigns diffthis HEAD^<CR>", { desc = "Diff This File (HEAD^)" })
+
+-- Diffview
+map("n", "<leader>dv", ":DiffviewOpen<CR>", { desc = "Open Git Diff View" })
+map("n", "<leader>dh", ":DiffviewFileHistory %<CR>", { desc = "File History" })
+map("n", "<leader>dr", ":DiffviewOpen origin/main...HEAD<CR>", { desc = "Diff with Remote (origin/main)" })
+map("n", "<leader>df", ":DiffviewOpen origin/main -- %<CR>", { desc = "Diff Current File with Remote" })
+map("n", "<leader>dp", ":DiffviewOpen HEAD^ HEAD<CR>", { desc = "Diff with Previous Commit" })
+map("n", "<leader>dc", ":DiffviewOpen HEAD^ -- %<CR>", { desc = "Diff Current File with Previous Commit" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,6 +36,31 @@ return {
                 changedelete = { text = "~" },
             },
             current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = "eol",
+                delay = 100,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+            },
+            current_line_blame_formatter = function(name, blame_info, opts)
+                if blame_info.author == name then
+                    blame_info.author = "You"
+                end
+                if blame_info.sha == "0000000000" or not blame_info.author then
+                    return { { "Not committed yet", "GitSignsBlame" } }
+                end
+                local date = os.date("%Y-%m-%d", blame_info.author_time)
+                return {
+                    {
+                        string.format("%s • %s • %s", blame_info.author, date, blame_info.summary),
+                        "GitSignsBlame",
+                    },
+                }
+            end,
+            diff_opts = {
+                algorithm = "patience",
+            },
         },
     },
     {
@@ -52,6 +77,31 @@ return {
                     ui = "String",
                     search = "DiffChange",
                     replace = "DiffAdd",
+                },
+            }
+        end,
+    },
+    {
+        "sindrets/diffview.nvim",
+        cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("diffview").setup {
+                enhanced_diff_hl = true,
+                view = {
+                    default = {
+                        layout = "diff2_horizontal",
+                        winbar_info = true,
+                    },
+                    merge_tool = {
+                        layout = "diff2_horizontal",
+                        disable_diagnostics = true,
+                    },
+                },
+                keymaps = {
+                    view = {
+                        ["<leader>c"] = ":DiffviewClose<CR>",
+                    },
                 },
             }
         end,
